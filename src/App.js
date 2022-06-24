2ac179b40858e7f52a860c06914cf990c92855a6
taha020 <taharasheed020@gmail.com>
2022-06-25 00:20:18 +0500

    Design Done, Header left

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from "react";
 
+//Importing Sections
+import Topbanner from "./Sections/Topbanner/topbanner";
+import Objectives from "./Sections/Objectives/objectives";
+import Projects from "./Sections/project/projects";
+import Skills from "./Sections/Skills/Skills";
+import Educaton from "./Sections/Education/educaton";
+import Contact from "./Sections/Contact/contact";
+import Footer from "./Sections/footer/footer";
+
 function App() {
   return (
     <div className="App">
-    <h2>My portFolio</h2>
+   
+    <Topbanner/>
+    <Objectives/>
+    <Projects/>
+    <Skills/>
+    <Educaton/>
+    <Contact/>
+    <Footer/>
+
     </div>
   );
 }
